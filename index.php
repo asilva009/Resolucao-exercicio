46e9eee6243b840b0af1d73ec554ff81e45821d7
asilva009 <asilva009@yahoo.com.br>
2020-09-11 18:15:46 -0300

    correção final

--- index.php
+++ index.php
@@ -1,45 +1,47 @@
 <?php
 function textWrap(string $text, int $length): array {
-    $resultado = []; // array com resultado desejado
-    $caracter = []; // array da string de entrada
-    $posicaoCaracter = 1; //posição do caracter na linha
-    $posicao = 0; // posição das linhas do array resultado
-    $resultado[$posicao] = ''; // cada linha do array resultado
-    
-    // loop para criar o array da string
+    $resultado = [];
+    $caracter = [];
+    $posicaoCaracter = 1;
+    $posicao = 0;
+    $resultado[$posicao] = '';
+    // Ajustando tamanho da string.
+    $text = utf8_decode($text);
+    // Loop para criar o array da string.
     for ($i = 0; $i < strlen($text); $i++) {
-        $caracter[$posicaoCaracter] = $text[$i];
-        $posicaoCaracter++;      
-      // verifica se a linha já atingiu o tamanho estipulado  
-        if (($posicaoCaracter - 1) == $length) {
-            // verifica se a linha tem espaços entre as palavras
-            if (in_array(' ', $caracter)) {
-                // verifica se o próximo caracter é ' '
-                if (isset($text[$i + 1]) && $text[$i + 1] != ' ') {
-                    // elimina ' ' da linha evitando corte de palavras
-                    for ($e = count($caracter); $e > 0; $e--) {
-                        if ($caracter[$e] == ' ') {
-                            $i -= $length - $e;
-                            array_pop($caracter);
-                            break;
-                        }    
-                        array_pop($caracter);     
-                    }
-                }              
+      $caracter[$posicaoCaracter] = $text[$i];
+      $posicaoCaracter++;
+      // Verifica se a linha já atingiu o tamanho estipulado.
+      if (($posicaoCaracter - 1) == $length) {
+        // Verifica se a linha tem espaços entre as palavras.
+        if (in_array(' ', $caracter)) {
+          // Verifica se o próximo caracter é ' '.
+          if (isset($text[$i + 1]) && $text[$i + 1] != ' ') {
+            // Elimina ' ' da linha evitando corte de palavras.
+            for ($e = count($caracter); $e > 0; $e--) {
+              if ($caracter[$e] == ' ') {
+                $i -= $length - $e;
+                array_pop($caracter);
+                break;
+              }
+              array_pop($caracter);
             }
-            if ($text[$i + 1] == ' ') {
-                $i += 1;
-            }        
-            // gera linha
-            $resultado[$posicao] = implode($caracter);
-            // prepara variáveis para iniciar uma nova linha
-            $posicaoCaracter = 1;
-            $caracter = [];
-            $posicao++;
-        // construção da última linha
-        }elseif ($i == (strlen($text)) - 1) {
-            $resultado[$posicao] = implode($caracter);                 
+          }
+          else {
+            $i += 1;
+          }
         }
+        // Gera linha.
+        $resultado[$posicao] = utf8_encode(implode($caracter));
+        // Prepara variáveis para iniciar uma nova linha.
+        $posicaoCaracter = 1;
+        $caracter = [];
+        $posicao++;
+        // Construção da última linha.
+      }
+      elseif ($i == (strlen($text)) - 1) {
+        $resultado[$posicao] = implode($caracter);
+      }
     }
     return $resultado;
 }
